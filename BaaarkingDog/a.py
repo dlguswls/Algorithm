518471be30fe7a0eed7d3eee9b90adf0e44d4731
Hyunjin Lee <dlskadn77@naver.com>
2023-06-19 22:55:00 +0900

    230619
    230619

--- BaaarkingDog/a.py
+++ BaaarkingDog/a.py
@@ -1,10 +1,37 @@
-def func1(n) :
+# def func1(n) :
+#     sum = 0
+#     for i in range(1, n+1) :
+#         if i % 3 == 0 :
+#             sum += i
+#             continue
+#         if i % 5 == 0 :
+#             sum += i
+#     return sum
+# print(func1(16))
+
+# def func2(arr, n) : 
+#     for i in range(n-1):
+#         for j in range(i+1, n) : 
+#             if arr[i]+arr[j] == 100 : 
+#                 return 1
+#     return 0
+# print(func2([4, 13, 63, 87],4))
+
+# def func3(n) : 
+#     temp = n**0.5
+#     if int(temp) == temp:
+#             return 1
+#     return 0
+
+# print(func3(756580036))
+
+
+def func4(n) : 
+    i = 0
     sum = 0
-    for i in range(1, n+1) :
-        if i % 3 == 0 :
-            sum`+= i
-            continue
-        if i % 5 == 0 :
-            sum += i
-    return sum
-print(func1(16))
+    while 2**i < n : 
+          i += 1
+    return 2**(i-1)
+    
+print(func4(97615282))
+
--- BaaarkingDog/first.c
+++ BaaarkingDog/first.c
@@ -1,8 +1,30 @@
-int func1(int arr[], int n){
-    int cnt = 0;
-    for(int i = 0; i < n; i++){
-        if(arr[i] % 5 == 0) cnt++;
-    }
-    return cnt;
+// #include <stdio.h>
+ 
+// int func1(int N){
+//     int ret = 0;
+//     for(int i = 1; i <= N; i++){
+//         if(i % 3 == 0 || i % 5 == 0) ret += i;
+//     }
+//     return ret;
+// }
+ 
+ 
+// int main(void) {
+//     printf("%d",func1(16));
+//     return 0;
+// }
+
+#include <stdio.h>
+ 
+int func2(int arr[], int N){
+    for(int i = 0; i < N; i++)
+        for (int j = i+1; j<N; j++)
+            if(arr[i] + arr[j] == 100) return 1;
+    return 0;
+}
+ 
+ 
+int main(void) {
+    printf("%d",func2({1, 52, 48},3));
 }
 
